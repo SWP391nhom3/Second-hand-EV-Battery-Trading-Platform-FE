10bee7132a97c155d2bdb38e398777d8da8d5ddb
DuongDinhKhoi-2607 <khoiddse182046@fpt.edu.vn>
2025-10-27 22:44:49 +0700

    đổi đường dẫn admin

---
+++ src/pages/admin/AdminPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Dashboard from "../../components/dashboard/Dashboard";
+
+const AdminPage = () => {
+  return (
+    <dashboard>
+      <Dashboard />
+    </dashboard>
+  );
+};
+
+export default AdminPage;
